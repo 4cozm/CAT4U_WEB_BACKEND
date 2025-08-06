7221b07a8b77340439bab1f8e48bb8d7cbf59315
4cozm <17328rm@gmail.com>
2025-08-06 17:12:49 +0900

    ♻️ express모듈 로딩 함수화 및 로딩 시점 변경

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 //서버 구동 없이 Express 객체만 반환 , 즉 앱 내부 설정
 import express from 'express';
 import path from 'path';
+import { sessionMiddleware } from './middlewares/sessionMiddleware.js';
 import router from './routes/index.js';
 
 const __dirname = path.resolve();
 const frontendPath = path.join(__dirname, 'public', 'frontend');
 
-const app = express();
+export function createApp() {
+    const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api', router);
-app.use(express.static(frontendPath));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use('/api', router);
+    app.use(express.static(frontendPath));
+    app.use(sessionMiddleware());
 
-app.use((req, res) => {
-    res.status(404).send('Not Found');
-});
-
-export default app;
+    return app;
+}
