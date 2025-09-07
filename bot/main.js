e09c73fc09734c199de2d8a54ef0aeeae4b53f17
root060 <chanhyeok060217@gmail.com>
2025-09-07 21:54:35 +0900

    unnecessary code delete

--- bot/main.js
+++ bot/main.js
@@ -1,5 +1,4 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
-import express from 'express';
 import dotenv from 'dotenv';
 import cron from 'node-cron'; // nodejs 스캐줄러
 
@@ -36,16 +35,3 @@ async function GetUserlist() {
 }
 
 client.login(token);
-
-const app = express();
-const PORT = 3000;
-
-// OAuth2 Redirect Callback 엔드포인트
-app.get('/auth/discord/callback', (req, res) => {
-    const code = req.query.code;
-    res.send('로그인 성공! Code: ' + code);
-});
-
-app.listen(PORT, () => {
-    console.log(`🌐 Web server running at http://localhost:${PORT}`);
-});
