4a997ee692a6d37876d826f9c2681ce4c2b9886a
4cozm <17328rm@gmail.com>
2025-09-18 19:49:25 +0900

    ⚰️ 리팩터링에 의해 사용하지 않는 아티팩트 코드 삭제 및 정리

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 //서버 구동 없이 Express 객체만 반환 , 즉 앱 내부 설정
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import path from 'path';
 import requireAuth from './middlewares/requireAuth.js';
 import { createSessionMiddleware } from './middlewares/sessionMiddleware.js';
 import router from './routes/index.js';
-
-const __dirname = path.resolve();
-const frontendPath = path.join(__dirname, 'public', 'frontend');
 
 export async function createApp() {
     const app = express();
@@ -18,8 +14,6 @@ export async function createApp() {
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-
-    app.use(express.static(frontendPath));
 
     app.use('/api', router);
     return app;
--- src/routes/githubArtifactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { downloadLatestArtifact } from '../controllers/githubArtifactController.js';
-
-const artifactRouter = express.Router();
-
-artifactRouter.post('/downloadLatestArtifact', downloadLatestArtifact);
-
-export default artifactRouter;
